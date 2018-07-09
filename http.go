d782507b5715e6ef8a9c33d30708c29da83d9360
Simon Eisenmann <s.eisenmann@kopano.com>
2018-07-09 15:36:30 +0200

    Bump default MaxIdleConnsPerHost to 100
    Since usually a client connects to the same Kopano server backend, this
    change allow reusing of more simultaneous connections to the same host,
    (was 2, now 100).

--- http.go
+++ http.go
@@ -34,6 +34,7 @@ var DefaultHTTPClient = &http.Client{
 			DualStack: true,
 		}).DialContext,
 		MaxIdleConns:          100,
+		MaxIdleConnsPerHost:   100,
 		IdleConnTimeout:       90 * time.Second,
 		TLSHandshakeTimeout:   10 * time.Second,
 		ExpectContinueTimeout: 1 * time.Second,
