46c796c477eacb548c16cc005413a344b022d34e
Simon Eisenmann <s.eisenmann@kopano.com>
2018-07-12 11:54:27 +0200

    Add various environment variables for HTTP client
    This change allows control of most settings of the internal HTTP client
    with environment variables.

--- http.go
+++ http.go
@@ -18,25 +18,94 @@
 package kcc
 
 import (
+	"fmt"
 	"net"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
 )
 
+// Default HTTP client settings.
+var (
+	DefaultHTTPTimeoutSeconds         int64 = 10
+	DefaultHTTPMaxIdleConns                 = 100
+	DefaultHTTPMaxIdleConnsPerHost          = 100
+	DefaultHTTPIdleConnTimeoutSeconds int64 = 90
+	DefaultHTTPDialTimeoutSeconds     int64 = 30
+	DefaultHTTPKeepAliveSeconds       int64 = 120
+	DefaultHTTPDualStack                    = true
+)
+
 // DefaultHTTPClient is the default Client as used by KCC for HTTP SOAP requests.
-var DefaultHTTPClient = &http.Client{
-	Timeout: 10 * time.Second,
-	Transport: &http.Transport{
-		Proxy: http.ProxyFromEnvironment,
-		DialContext: (&net.Dialer{
-			Timeout:   30 * time.Second,
-			KeepAlive: 120 * time.Second,
-			DualStack: true,
-		}).DialContext,
-		MaxIdleConns:          100,
-		MaxIdleConnsPerHost:   100,
-		IdleConnTimeout:       90 * time.Second,
+var DefaultHTTPClient *http.Client
+
+func init() {
+	debug = os.Getenv("KCC_GO_DEBUG") != ""
+
+	if s := os.Getenv("KCC_GO_HTTP_TIMEOUT"); s != "" {
+		if n, err := strconv.ParseInt(s, 10, 64); err == nil {
+			DefaultHTTPTimeoutSeconds = n
+		}
+	}
+	if s := os.Getenv("KCC_GO_HTTP_MAX_IDLE_CONNS"); s != "" {
+		if n, err := strconv.ParseInt(s, 10, 0); err == nil {
+			DefaultHTTPMaxIdleConns = int(n)
+		}
+	}
+	if s := os.Getenv("KCC_GO_HTTP_MAX_IDLE_CONNS_PER_HOST"); s != "" {
+		if n, err := strconv.ParseInt(s, 10, 0); err == nil {
+			DefaultHTTPMaxIdleConnsPerHost = int(n)
+		}
+	}
+	if s := os.Getenv("KCC_GO_HTTP_IDLE_CONN_TIMEOUT"); s != "" {
+		if n, err := strconv.ParseInt(s, 10, 64); err == nil {
+			DefaultHTTPIdleConnTimeoutSeconds = n
+		}
+	}
+	if s := os.Getenv("KCC_GO_HTTP_DIAL_TIMEOUT"); s != "" {
+		if n, err := strconv.ParseInt(s, 10, 64); err == nil {
+			DefaultHTTPDialTimeoutSeconds = n
+		}
+	}
+	if s := os.Getenv("KCC_GO_HTTP_KEEPALIVE"); s != "" {
+		if n, err := strconv.ParseInt(s, 10, 64); err == nil {
+			DefaultHTTPKeepAliveSeconds = n
+		}
+	}
+	if s := os.Getenv("KCC_GO_HTTP_DUALSTACK"); s != "" {
+		switch s {
+		case "off", "false", "no":
+			DefaultHTTPDualStack = false
+		case "on", "true", "yes":
+			DefaultHTTPDualStack = true
+		}
+	}
+
+	dialer := &net.Dialer{
+		Timeout:   time.Duration(DefaultHTTPDialTimeoutSeconds) * time.Second,
+		KeepAlive: time.Duration(DefaultHTTPKeepAliveSeconds) * time.Second,
+		DualStack: DefaultHTTPDualStack,
+	}
+
+	transport := &http.Transport{
+		Proxy:                 http.ProxyFromEnvironment,
+		DialContext:           dialer.DialContext,
+		MaxIdleConns:          DefaultHTTPMaxIdleConns,
+		MaxIdleConnsPerHost:   DefaultHTTPMaxIdleConnsPerHost,
+		IdleConnTimeout:       time.Duration(DefaultHTTPIdleConnTimeoutSeconds) * time.Second,
 		TLSHandshakeTimeout:   10 * time.Second,
 		ExpectContinueTimeout: 1 * time.Second,
-	},
+	}
+
+	DefaultHTTPClient = &http.Client{
+		Timeout:   time.Duration(DefaultHTTPTimeoutSeconds) * time.Second,
+		Transport: transport,
+	}
+
+	if debug {
+		fmt.Printf("HTTP client: %+v\n", DefaultHTTPClient)
+		fmt.Printf("HTTP client transport: %+v\n", transport)
+		fmt.Printf("HTTP client transport dial: %+v\n", dialer)
+	}
 }
